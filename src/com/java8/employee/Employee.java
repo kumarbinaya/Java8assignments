e13465b6b346e69fbf6bdb986443e4d345bf2267
kumarbinaya <binayab@emids.com>
2021-07-16 08:28:28 +0530

    Initial Commit:Basic Employee class structure

---
+++ src/com/java8/employee/Employee.java
@@ -0,0 +1,41 @@
+package com.java8.employee;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    private double salary;
+    private String department;
+}
